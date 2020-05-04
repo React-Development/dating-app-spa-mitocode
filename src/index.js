ec5c80603cc142efd0f6dd673721c5b57d423fb0
Brayan Garcia <bgarcia.95@outlook.com>
2020-05-04 16:42:24 -0600

    Adding bootswatch, reactstrap, bootstrap and font awesome into the project.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+// importing bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+// importing font-awesome
+import "font-awesome/css/font-awesome.min.css";
+// importing bootswatch
+import "bootswatch/dist/lumen/bootstrap.min.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
